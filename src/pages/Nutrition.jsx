1c14815363d945e91d2b6b5ffa4d449e8a03bc3c
yablokimedovie <149507913+yablokimedovie@users.noreply.github.com>
2025-05-14 08:55:46 +0000

    зміни 5

--- src/pages/Nutrition.jsx
+++ src/pages/Nutrition.jsx
@@ -2,12 +2,13 @@ function Nutrition() {
   return (
     <main className="container px-4 py-4 flex-grow-1">
       <section>
-        <h2 className="h2 text-success mb-4">Харчування зайців</h2>
-        <p>Зайці є травоїдними тваринами. Їх раціон включає:</p>
+        <h2 className="h2 text-success mb-4">Харчування Сірих вовків</h2>
+        <p>Вовк — типовий хижак, що добуває їжу самостійно активним пошуком та переслідуванням жертв. Їх раціон включає:</p>
         <ul className="list-group">
-          <li className="list-group-item">Траву, листя і кору дерев.</li>
-          <li className="list-group-item">Овочі, такі як морква та капуста.</li>
-          <li className="list-group-item">Воду, яку вони споживають у великих кількостях.</li>
+          <li className="list-group-item">В тундрі — дикі та свійські північні олені.</li>
+          <li className="list-group-item">В лісовій зоні — лосі, сарни, дикі свині, свійські вівці, корови, коні.</li>
+          <li className="list-group-item">В степу та пустелі — антилопи різних видів та вівці.</li>
+          <li className="list-group-item">В горах — дикі та свійські кози.</li>
         </ul>
       </section>
     </main>
